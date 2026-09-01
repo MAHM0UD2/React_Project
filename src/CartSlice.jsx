1300b6dbd7d72fd5c730d5b079b008c920333456
Mahmoud <maal00013@stud.uni-saarland.de>
2026-09-01 15:33:39 -0400

    CartSlice implemented

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,23 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const plant = action.payload;
+        const exist = state.items.find(e => e.name === plant.name);
+        if (exist) {
+            exist.quantity++;
+        } else {
+            state.items.push({...plant, quantity: 1});
+        }
     },
     removeItem: (state, action) => {
+        state.items = state.items.filter(e => e.name !== action.payload);
     },
     updateQuantity: (state, action) => {
-
-    
+        const {name, amount} = action.payload;
+        const exist = state.items.find(e => e.name === name);
+        if (exist) {
+            exist.quantity += amount;
+        }
     },
   },
 });
