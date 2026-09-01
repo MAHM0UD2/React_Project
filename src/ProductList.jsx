f301fca4ab1f4fadb409e883c4a254a6c9c4ca44
Mahmoud <maal00013@stud.uni-saarland.de>
2026-09-01 14:56:06 -0400

    handlAddToCart implemented

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import addItem from './CartItem';
+import addItem from './CartSlice';
+import { useSelector, useDispatch } from "react-redux";
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const cartItems = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -256,7 +259,11 @@ function ProductList({ onHomeClick }) {
     };
 
     const handleAddToCart = (plant) => {
-        
+        dispatch(addItem(plant));
+        setAddedToCart((prevState) => ({
+            ...prevState,
+            [plant.name]: true,
+        }));
     };
     return (
         <div>
